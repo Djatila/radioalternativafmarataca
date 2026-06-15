003df211f4b1598ed9fa0c9d84a94e304143961f
Djatila <125503399+Djatila@users.noreply.github.com>
2026-06-15 13:51:14 -0300

    feat: transforma radio em PWA instalável com manifest, sw e ícones

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'radio-alternativa-v1';
+const ASSETS = [
+    './radio_vozdacity (1).html',
+    './manifest.json',
+    './icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
